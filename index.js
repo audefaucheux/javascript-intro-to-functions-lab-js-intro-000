69cd845b3c8bc7eb4f2afa018ecd731953723247
Aude Faucheux <aude53@gmail.com>
2019-05-12 10:47:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!";
   } else if (string.toUpperCase()) {
     return "YES INDEED!";
-  } else if (sting === "I love you, Grandma.") {
+  } else (sting === "I love you, Grandma.") {
     return "I love you, too.";
   }
 }
